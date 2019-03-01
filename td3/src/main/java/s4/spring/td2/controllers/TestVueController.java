385855a1c16725fe2e2380955b04c6b4019c5f16
Quentin Lemoigne <21700899@etu.unicaen.fr>
2019-03-01 16:05:00 +0100

    Fin td3

--- td3/src/main/java/s4/spring/td2/controllers/TestVueController.java
+++ td3/src/main/java/s4/spring/td2/controllers/TestVueController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.github.jeemv.springboot.vuejs.VueJS;
+import io.github.jeemv.springboot.vuejs.utilities.Http;
 
 @Controller
 @RequestMapping("/vue")
@@ -17,8 +21,21 @@ public class TestVueController {
 	@GetMapping("test")
 	public String test(Model model ) {
 		model.addAttribute("message", "HEllo world!");
-		return "vueJS/test";
+		vue.addMethod("update", "this.message='Message modifié!';");
+		vue.addMethod("testAjax","var self=this;"+ Http.post("/vue/test/ajax","{v:self.inputValue}",
+				"self.ajaxMessage=response.data;self.alertVisible=true"));
+		vue.addDataRaw("message", "Hello world!");
+		vue.addData("alertVisible", false);
+		vue.addData("ajaxMessage");
+		vue.addData("inputValue");
+		return "vueJs/test";
 		
+	}
+	
+	@PostMapping("test/ajax")
+	@ResponseBody
+	public String testAjax (@RequestBody String v) {
+		return "Test ok!"+v;
 	}
 
 }
